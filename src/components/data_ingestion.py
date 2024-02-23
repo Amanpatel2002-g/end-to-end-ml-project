f34bc6df08e16076cc8b13d11f4647bb0ad75d1d
apoff <apofficial2002@gmail.com>
2024-02-23 22:02:25 +0530

    video5 completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', 'train.csv')
@@ -45,4 +47,9 @@ if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    modelTrainer = ModelTrainer()
+    r2 = modelTrainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr)
+    print(r2)
+    
