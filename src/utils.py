4030a9f586904fdbdaf376c732b9b1c7a343d4de
apoff <apofficial2002@gmail.com>
2024-02-24 18:13:38 +0530

    video6 completed

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import os, sys
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
 from src. exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from src.logger import logging
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -13,18 +15,33 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaulate_model(X_train, y_train, X_test, y_test, models):
+def evaulate_model(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
-        
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
+
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            report[(list(models.keys()))[i]] = test_model_score
+
+            report[list(models.keys())[i]] = test_model_score
+            logging.info(f"Hyperparameter tuning done on the {list(models.keys())[i]} model and test model score is saved to report")
+
         return report
     except Exception as e:
         raise CustomException(e, sys)
